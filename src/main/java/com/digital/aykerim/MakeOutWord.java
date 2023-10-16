bca1ad954e18bfbe258621146b7562ba0ece8c3c
Aykerim <aika_sony@mail.ru>
2023-10-16 20:46:43 +0600

    feature:addedMakeOutWord

--- src/main/java/com/digital/aykerim/MakeOutWord.java
+++ src/main/java/com/digital/aykerim/MakeOutWord.java
@@ -1,2 +1,9 @@
 package com.digital.aykerim;public class MakeOutWord {
+
+    public static void main(String[] args) {
+        System.out.println(makeOutWord("<<>>", "Yay"));
+    }
+    public static String makeOutWord(String out, String word) {
+        return out.substring(0,2) + word + out.substring(2,4);
+    }
 }
