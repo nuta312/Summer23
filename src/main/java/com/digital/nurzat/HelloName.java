5ce406d2fa97153c233e2ef76b3614603c3239ad
Aibike <aibikekimanbaeva@gmail.com>
2023-10-16 13:15:36 +0600

    resolve conflicts in nurzat pack

--- src/main/java/com/digital/nurzat/HelloName.java
+++ src/main/java/com/digital/nurzat/HelloName.java
@@ -1,12 +1,8 @@
 package com.digital.nurzat;
 
 public class HelloName {
-    public static void main(String[] args) {
-
-<<<<<<< HEAD
-=======
+    public static void main(String[] args)
         String name = "Nurzat";
         System.out.println("Hello " + name + '!');
->>>>>>> origin
     }
 }
