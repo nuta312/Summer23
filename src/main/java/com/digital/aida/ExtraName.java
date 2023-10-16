3a1a08180ec1f9f4e6999595346171d6338940c7
Aida-QA <aida.sadyrkulova85@gmail.com>
2023-10-16 14:59:36 +0600

    added thirdbranch

--- src/main/java/com/digital/aida/ExtraName.java
+++ src/main/java/com/digital/aida/ExtraName.java
@@ -1,8 +1,13 @@
 package com.digital.aida;
 
 public class ExtraName {
-    public String extraEnd(String str) {
+    public static String extraEnd(String str) {
         String end = str.substring(str.length() - 2);
         return end + end + end;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(extraEnd("Extra end"));
+
     }
 }
