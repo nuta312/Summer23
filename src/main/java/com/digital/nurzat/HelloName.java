9ffaec26a2149d4f78f40a9262de775d7481d968
Ruslan <143115412+Ruslan-NOMAD@users.noreply.github.com>
2023-10-16 19:12:28 +0600

    :-)

--- src/main/java/com/digital/nurzat/HelloName.java
+++ src/main/java/com/digital/nurzat/HelloName.java
@@ -2,15 +2,11 @@ package com.digital.nurzat;
 
 public class HelloName {
     public static void main(String[] args) {
-<<<<<<< HEAD
 
 
         String name = "Nurzat";
         System.out.println("Hello " + name + '!');
 
-=======
-        String name = "Nurzat";
-        System.out.println("Hello " + name + '!');
->>>>>>> 8fde8f0688bdc53e2e6f5ea2f11eff8335287289
+
     }
 }
