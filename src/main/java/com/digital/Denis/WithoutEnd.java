7dfb4fc739f34b38dc8bd115957b0d1614b1cedb
Denis <yugai.denis17@gmail.com>
2023-10-16 16:06:27 +0600

    feature: addedwithoutEnd

--- src/main/java/com/digital/Denis/WithoutEnd.java
+++ src/main/java/com/digital/Denis/WithoutEnd.java
@@ -9,7 +9,6 @@ public class WithoutEnd {
         else
             return str.substring(1,str.length()-1);
     }
-
     public static void main(String[] args) {
         System.out.println(withoutEnd("java"));
     }
