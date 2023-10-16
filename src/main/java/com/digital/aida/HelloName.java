6a05a5faf1e976c1df4c2e3a4787827fa31e7117
Aibike <aibikekimanbaeva@gmail.com>
2023-10-16 19:46:31 +0600

    clean file in aida pack

--- src/main/java/com/digital/aida/HelloName.java
+++ src/main/java/com/digital/aida/HelloName.java
@@ -3,11 +3,5 @@ package com.digital.aida;
 public class HelloName {
     public String HelloName(String name) {
         return "Hello" + name + "!";
-
     }
     }
-
-<<<<<<< HEAD
-=======
-
->>>>>>> origin
