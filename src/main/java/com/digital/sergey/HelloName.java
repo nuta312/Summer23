2e41b0c6be7d3d6fea29afa810d926965fc02a44
Sergey <myphone.kg2@mail.ru>
2023-10-16 10:20:52 +0300

    feature:MakeAbba

--- src/main/java/com/digital/sergey/HelloName.java
+++ src/main/java/com/digital/sergey/HelloName.java
@@ -7,4 +7,6 @@ public class HelloName {
 
     }
 
+
+
 }
---
+++ src/main/java/com/digital/sergey/MakeAbba.java
@@ -0,0 +1,9 @@
+package com.digital.sergey;
+
+public class MakeAbba {
+    public String makeAbba(String a, String b) {
+        return a+b+b+a;
+    }
+
+}
+
