963cbea76f2c95035397e2ee7916d6f28ccc3543
Aida-QA <aida.sadyrkulova85@gmail.com>
2023-10-16 12:09:26 +0600

    feature:added make out word

--- src/main/java/com/digital/aida/MakeTags.java
+++ src/main/java/com/digital/aida/MakeTags.java
@@ -1,4 +1,9 @@
 package com.digital.aida;
 
 public class MakeTags {
+    public String makeTags(String tag, String word) {
+        String html = "<" + tag + ">" + word + "</" + tag + ">";
+        return html;
+
+    }
 }
