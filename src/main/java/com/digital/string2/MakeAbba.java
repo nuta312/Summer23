2d4adcf40f3220ab04e3471242f05fcf2873839f
Eliza <saparalieva.eliza2@gmail.com>
2023-10-16 16:41:45 +0600

    stringMakeAbba

--- src/main/java/com/digital/string2/MakeAbba.java
+++ src/main/java/com/digital/string2/MakeAbba.java
@@ -1,4 +1,14 @@
 package com.digital.string2;
 
 public class MakeAbba {
+    public static void main(String[] args) {
+        System.out.println(makeAbba("ali", "za"));
+    }
+    public static String makeAbba(String a, String b) {
+// String result = a + b + b + a;
+// return result;
+        return  a + b + b + a;
+
+
+    }
 }
