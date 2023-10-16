a423a1f0fa43d59d39c1be82a70ec25e8569582d
Urustamov Nur <nurzigit.0808@gmail.com>
2023-10-16 22:06:28 +0600

    feature: addfirstTwo

---
+++ src/main/java/com/digital/Nurjigit/FirtsTwo.java
@@ -0,0 +1,17 @@
+package com.digital.Nurjigit;
+
+public class FirtsTwo {
+    public static void main(String[] args) {
+        System.out.println(firstTwo("djhk"));
+    }
+    public static String firstTwo(aString str) {
+        if(str.length() < 2)
+            return str;
+
+        return str.substring(0, 2);
+
+
+
+
+    }
+}
