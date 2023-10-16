56583e4bcf0d14ba4323b36ae212ddf9ad99f80b
Urustamov Nur <nurzigit.0808@gmail.com>
2023-10-16 10:54:10 +0600

    feature:addedHelloName

---
+++ src/main/java/com/digital/Nurjigit.java
@@ -0,0 +1,8 @@
+package com.digital;
+
+public class Nurjigit {
+    public static void main(String[] args) {
+        String name = "Nuru";
+        System.out.println("Hello " + name + "!");
+    }
+}
