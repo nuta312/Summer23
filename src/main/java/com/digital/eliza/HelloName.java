dc188fc0f1e86905151e4e19a02c65ce61178a19
Eliza <saparalieva.eliza2@gmail.com>
2023-10-16 16:24:49 +0600

    firstString

---
+++ src/main/java/com/digital/eliza/HelloName.java
@@ -0,0 +1,10 @@
+package com.digital.eliza;
+
+public class HelloName {
+    public static void main(String[] args) {
+        System.out.println(helloName("Eliza"));
+    }
+    public  static String helloName(String name) {
+        return "Hello " + name + "!";
+    }
+}
