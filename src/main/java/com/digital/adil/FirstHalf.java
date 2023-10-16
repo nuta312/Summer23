075b2ae214c98e30164dbab27e68476bda95c835
Adil <adilemilbekov12@gmail.com>
2023-10-16 13:14:50 +0600

    feature: added first half

---
+++ src/main/java/com/digital/adil/FirstHalf.java
@@ -0,0 +1,16 @@
+package com.digital.adil;
+
+public class FirstHalf {
+
+    public class FirstHalf {
+        public static String firstHalf(String str) {
+            return str.substring(0,str.length()/2);
+
+        }
+
+        public static void main(String[] args) {
+            System.out.println(firstHalf("Tyson"));
+
+        }
+    }
+}
