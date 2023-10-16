60f3dfd2b249f662fc6b0daddafe83774e2ebe99
Kanybek <matisakov12@gmail.com>
2023-10-16 12:03:25 +0600

    Feature:makeTags

--- src/main/java/com/digital/kanybek/MakeTags.java
+++ src/main/java/com/digital/kanybek/MakeTags.java
@@ -1,2 +1,12 @@
-package com.digital.kanybek;public class MakeTags {
+package com.digital.kanybek;
+
+public class MakeTags {
+    public static void main(String[] args) {
+        MakeTags codingbat = new MakeTags();
+        System.out.println(codingbat.makeTags("i","Yay"));
+    }
+    public String makeTags(String tag, String word) {
+        return "<" + tag + ">" + word + "</" + tag + ">";
+    }
+
 }
