fae27849284d871252308d8a85b555451b1008db
Urustamov Nur <nurzigit.0808@gmail.com>
2023-10-16 16:38:25 +0600

    feature: AddedHelloName

---
+++ src/main/java/com/digital/Nurjigit.java
@@ -0,0 +1,9 @@
+package com.digital;
+
+public class Nurjigit {
+    public static void main(String[] args) {
+        String name = "Aibike sensey";
+        System.out.println("Hello " + name + "!");
+
+    }
+}
