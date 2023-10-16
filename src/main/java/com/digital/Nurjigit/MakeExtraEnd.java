76bebba24db33f1530f573a5ff2118217c396454
Urustamov Nur <nurzigit.0808@gmail.com>
2023-10-16 20:35:24 +0600

    feature:AddedExtraEnd

---
+++ src/main/java/com/digital/Nurjigit/MakeExtraEnd.java
@@ -0,0 +1,9 @@
+package com.digital.Nurjigit;
+
+public class MakeExtraEnd {
+    public static void main(String[] args) {
+        String str = "Nuru";
+        String end = str.substring(str.length()-2);
+        System.out.println(end+end+end);
+    }
+}
