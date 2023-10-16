9bf4fcb86c962421d1c409c01db4aed46bfdd2fb
Tihinovskii <tihonovskii94@gmail.com>
2023-10-16 17:36:21 +0300

    feature added/MakeOutWord

---
+++ src/main/java/com/digital/andrey/MakeOutWord.java
@@ -0,0 +1,8 @@
+package com.digital.andrey;
+
+public class MakeOutWord {
+    public String makeOutWord(String out, String word) {
+        return out.substring(0,2) + word + out.substring(2,4);
+    }
+
+}
--- src/main/java/com/digital/andrey/MakeTags.java
+++ src/main/java/com/digital/andrey/MakeTags.java
@@ -1,4 +1,7 @@
 package com.digital.andrey;
 
 public class MakeTags {
+    public String makeTags(String teg, String word){
+        return "<" + teg +">" + word;
+    }
 }
