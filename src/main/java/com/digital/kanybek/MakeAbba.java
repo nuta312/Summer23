6e6fbd82263f147efd1544b7bb4e2a825d3558cc
Kanybek <matisakov12@gmail.com>
2023-10-16 11:47:14 +0600

    Feature:makeAbba

--- src/main/java/com/digital/kanybek/MakeAbba.java
+++ src/main/java/com/digital/kanybek/MakeAbba.java
@@ -1,2 +1,11 @@
-package com.digital.kanybek;public class MakeAbba {
+package com.digital.kanybek;
+
+public class MakeAbba {
+    public static void main(String[] args) {
+        MakeAbba codingbat = new MakeAbba();
+        System.out.println(codingbat.makeAbba("Hi","Bye"));
+    }
+    public String makeAbba(String a, String b) {
+        return a + b + b + a;
+    }
 }
