647989eb4300e53e0e662bac109c4d2ebec92b37
Ruslan <143115412+Ruslan-NOMAD@users.noreply.github.com>
2023-10-16 12:03:03 +0600

    Fix: deleted other files

--- src/main/java/com/digital/nurzat/HelloName.java
+++
@@ -1,7 +0,0 @@
-package com.digital.nurzat;
-
-public class HelloName {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/main/java/org/example/rus/ExtraEnd.java
@@ -0,0 +1,12 @@
+package org.example.rus;
+
+public class ExtraEnd {
+    public static String extraEnd(String str) {
+        String end = str.substring(str.length()-2);
+        return end + end+ end;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(extraEnd("Hello"));
+    }
+}
