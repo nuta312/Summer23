c29bf459e6ec1d55bca52b93e14bf70bbff95450
Aykerim <aika_sony@mail.ru>
2023-10-16 19:27:36 +0600

    feature: addedHelloName

--- src/main/java/com/digital/aykerim/HelloName.java
+++ src/main/java/com/digital/aykerim/HelloName.java
@@ -1,2 +1,7 @@
-package com.digital.aykerim;public class HelloName {
+package com.digital.aykerim;
+
+public class HelloName {
+    public static void main(String[] args) {
+        System.out.println("Hello Aykerim");
+    }
 }
