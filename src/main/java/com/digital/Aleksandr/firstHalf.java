6d8c5694d2ef952ba39791bb0b9eecf85f8c05f0
Alexandr <udenkosana10@gmail.com>
2023-10-17 20:38:12 +0600

    feature:addedFirstHalf

---
+++ src/main/java/com/digital/Aleksandr/firstHalf.java
@@ -0,0 +1,9 @@
+package com.digital.Aleksandr;
+
+public class firstHalf {
+    public String firstHalf(String str) {
+        int word2 = str.length()/2;
+        return str.substring(0,word2);
+    }
+
+}
