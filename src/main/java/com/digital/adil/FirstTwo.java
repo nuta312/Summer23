a297b90b1e0587d3b37595b024191bacd08dae00
Adil <adilemilbekov12@gmail.com>
2023-10-17 10:13:34 +0600

    added Non start

--- src/main/java/com/digital/adil/FirstTwo.java
+++ src/main/java/com/digital/adil/FirstTwo.java
@@ -1,12 +1,11 @@
 package com.digital.adil;
 
-<<<<<<< HEAD
 
-    public class FirstTwo {
-=======
+
+
 public class FirstTwo {
 
->>>>>>> master
+
         public static String firstTwo(String str) {
             if (str.length() >=3)
                 return str.substring (0,2);
