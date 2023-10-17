6befc14cc9d063cd5b44a957bf8a3bc776ba833b
Нургазы Оморбеков <n.omorbekov@saimatelecom.kg>
2023-10-17 23:07:14 +0600

    feature: added / Right2

---
+++ src/main/java/com/digital/nurgazy/Right2.java
@@ -0,0 +1,7 @@
+package com.digital.nurgazy;
+
+public class Right2 {
+    public String right2(String str) {
+        return str.substring(str.length()-2) + str.substring(0, str.length()-2);
+    }
+}
