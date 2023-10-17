c2439a55903bd5be6514c2fe685be09c627aa635
Sasha <udenkosana10gmail.com>
2023-10-17 18:52:04 +0600

    feature:FrontTimes

--- src/main/java/com/digital/Sasha/countXX.java
+++ src/main/java/com/digital/Sasha/countXX.java
@@ -1,2 +1,13 @@
-package com.digital.Sasha;public class countXX {
+package com.digital.Sasha;
+
+public class countXX {
+    int countXX(String str) {
+        int result = 0;
+        for(int i =0;i<str.length()-1;i++){
+            if(str.substring(i,i+2).equals("xx")){
+                result ++;
+            }
+        } return result;
+    }
+
 }
--- src/main/java/com/digital/Sasha/frontTimes.java
+++ src/main/java/com/digital/Sasha/frontTimes.java
@@ -1,2 +1,20 @@
-package com.digital.Sasha;public class frontTimes {
+package com.digital.Sasha;
+
+public class frontTimes {
+    public String frontTimes(String str, int n) {
+        String result ="";
+        if (str.length()<3){
+            for(int i=0;i<n;i++){
+                result= result+str;
+            }
+
+        }else{
+            for(int i=0; i<n; i++){
+                result +=str.substring(0,3);
+            }
+
+        }
+        return result;
+
+    }
 }
