16641a75fff65e0ab1b1313f963e4dd3a404adc4
Urustamov Nur <nurzigit.0808@gmail.com>
2023-10-17 15:26:23 +0600

    feature: AddedWithotEnd

---
+++ src/main/java/com/digital/Nurjigit/WithoutEnd.java
@@ -0,0 +1,11 @@
+package com.digital.Nurjigit;
+
+public class WithoutEnd {
+    public static void main(String[] args) {
+        System.out.println(withoutEnd("Porsche"));
+    }
+    public static String withoutEnd(String str) {
+        return str.substring(1, str.length()-1);
+    }
+
+}
