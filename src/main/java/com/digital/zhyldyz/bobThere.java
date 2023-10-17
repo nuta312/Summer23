78c2a980f20b3c7ce7685b737cd1c98b4775550f
Zhyldyz Zheenbaeva <zhyldyzzheenbaeva@MacBook-Air-Zhyldyz.local>
2023-10-17 23:12:11 +0600

    bobThere

--- src/main/java/com/digital/zhyldyz/bobThere.java
+++ src/main/java/com/digital/zhyldyz/bobThere.java
@@ -1,12 +1,16 @@
 package com.digital.zhyldyz;
 
 public class bobThere {
-    public boolean bobThere(String str) {
+    public static boolean bobThere(String str) {
 
         for (int i=0; i< str.length()-2; i++){
             if  (str.charAt(i)=='b'&& str.charAt(i+2)=='b'){
                 return true;
             }
         }return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(bobThere("srt"));
     }
 }
