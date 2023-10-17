7d32a3645644694e2192495b43aef92e6a755827
ElmiraK <elmikyd@gmail.com>
2023-10-17 13:25:06 +0600

    feature:HelloName

--- src/main/java/com/digital/elmira/HelloName.java
+++ src/main/java/com/digital/elmira/HelloName.java
@@ -3,7 +3,6 @@ public class HelloName {
    public static String helloName(String name) {
        return "Hello" + ' ' + name + "!";
    }
-
     public static void main(String[] args) {
         System.out.println(helloName("Bob"));
     }
