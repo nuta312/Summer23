cb68b2e6b22645a4492225863442222f8bbcde2f
Ruslan <143115412+Ruslan-NOMAD@users.noreply.github.com>
2023-10-17 09:13:57 +0600

    kf

--- src/main/java/com/digital/Rus/EtiOshibkiZapariliCestno.java
+++ src/main/java/com/digital/Rus/EtiOshibkiZapariliCestno.java
@@ -4,5 +4,7 @@ public class EtiOshibkiZapariliCestno {
     public static void main(String[] args) {
         System.out.println("da zapparil chesno");
         System.out.printf("df");
+        // gtgsksdfjgkjadshjkghdfjkdfzbdf
+//        kfghzkljdfnblziudhzkdfn
     }
 }
