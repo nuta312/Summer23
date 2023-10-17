3d178eed59a8f65c9e437712d56b8de30029eb3c
Alexandr <udenkosana10@gmail.com>
2023-10-17 19:58:48 +0600

    feature:addedTask

--- src/main/java/com/digital/Alexandr/no9.java
+++ src/main/java/com/digital/Alexandr/no9.java
@@ -1,2 +1,17 @@
-package com.digital.Alexandr;public class no9 {
+package com.digital.Alexandr;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class no9 {
+    public List<Integer> no9(List<Integer> nums) {
+        return nums.stream().filter(this::delit).collect(Collectors.toList());
+        // nums.removeIf(n-> n%10==9);
+        // return nums;
+
+    }
+    public boolean delit(int n){
+        return n%10 !=9;
+    }
+
 }
