396e237c74bb2bf131377550a631b11433d61d6a
Ruslan <143115412+Ruslan-NOMAD@users.noreply.github.com>
2023-10-17 10:54:56 +0600

    pop

--- src/main/java/com/digital/rus/String.java
+++ src/main/java/com/digital/rus/String.java
@@ -3,5 +3,6 @@ package com.digital.rus;
 public class String {
     public static void main(String[] args) {
         System.out.println("all deleted");
+        System.out.println("all small");
     }
 }
